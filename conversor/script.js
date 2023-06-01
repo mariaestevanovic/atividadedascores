82015712ff8aaf2f7da9cab6a658141458cbd575
Maria Clara Oliveira Estevanovic Moura <65196712+mariaestevanovic@users.noreply.github.com>
2023-06-01 07:19:12 -0300

    Add files via upload

---
+++ conversor/script.js
@@ -0,0 +1,35 @@
+function convertTemperature() {
+
+    var temperatureInput = document.getElementById("temperature").value;
+    
+    if (temperatureInput) {
+     
+      var temperatureCelsius = parseFloat(temperatureInput);
+      
+      var temperatureFahrenheit = (temperatureCelsius * 9) / 5 + 32;
+  
+      var temperatureKelvin = temperatureCelsius + 273.15;
+  
+      var result = document.getElementById("result");
+      result.innerHTML =
+        "Temperatura em Celsius: " +
+        temperatureCelsius.toFixed(2) +
+        "°C<br />" +
+        "Temperatura em Fahrenheit: " +
+        temperatureFahrenheit.toFixed(2) +
+        "°F<br />" +
+        "Temperatura em Kelvin: " +
+        temperatureKelvin.toFixed(2) +
+        "K";
+  
+      var body = document.getElementsByTagName("body")[0];
+  
+      if (temperatureCelsius > 30) {
+        body.style.backgroundColor = "#191970";
+      } else if (temperatureCelsius >= 10 && temperatureCelsius <= 30) {
+        body.style.backgroundColor = "#7FFFD4";
+      } else {
+        body.style.backgroundColor = "#DAA520";
+      }
+    }
+  }
